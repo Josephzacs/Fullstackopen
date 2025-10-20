a6477029814affb5682de69922566fd21826facb
Josephzacs <95284376+Josephzacs@users.noreply.github.com>
2025-10-20 20:15:22 -0300

    unicafe, paso 3

--- part1/unicafe/part1/src/App.jsx
+++ part1/unicafe/part1/src/App.jsx
@@ -31,45 +31,54 @@ const Metricas = ({good, neutral, bad, all, average, positive}) => {
 
 const App = () => {
   // guardar los clics de cada boton en un estado
+  const [metricas , setMetricas] = useState({
+    all: 0,
+    average: 0,
+    positive: 0
+  })
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
+
   
   const handleClickGood = () => {
     const newGood = good + 1
-    const newAll = all + 1
+    const newAll = metricas.all + 1
     const newAverage = newAll === 0 ? 0 : (newGood - bad) / newAll
     const newPositive = newAll === 0 ? 0 : (newGood / newAll) * 100
 
     setGood(newGood)
-    setAll(newAll)
-    setAverage(newAverage)
-    setPositive(newPositive)
+    setMetricas({
+      all: newAll,
+      average: newAverage,
+      positive: newPositive
+    })
   }
   const handleClickNeutral = () => {
     const newNeutral = neutral + 1
-    const newAll = all + 1
+    const newAll = metricas.all + 1
     const newAverage = newAll === 0 ? 0 : (good - bad) / newAll
     const newPositive = newAll === 0 ? 0 : (good / newAll) * 100
 
     setNeutral(newNeutral)
-    setAll(newAll)
-    setAverage(newAverage)
-    setPositive(newPositive)
+    setMetricas({
+      all: newAll,
+      average: newAverage,
+      positive: newPositive
+    })
   }
   const handleClickBad = () => {
     const newBad = bad + 1
-    const newAll = all + 1
+    const newAll = metricas.all + 1
     const newAverage = newAll === 0 ? 0 : (good - newBad) / newAll
     const newPositive = newAll === 0 ? 0 : (good / newAll) * 100
 
     setBad(newBad)
-    setAll(newAll)
-    setAverage(newAverage)
-    setPositive(newPositive)
+    setMetricas({
+      all: newAll,
+      average: newAverage,
+      positive: newPositive
+    })
   }
   
 
@@ -79,7 +88,7 @@ const App = () => {
       <Button onClick={handleClickGood} text="good"/>
       <Button onClick={handleClickNeutral} text="neutral"/>
       <Button onClick={handleClickBad} text="bad"/>
-      <Metricas good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive}/>
+      <Metricas good={good} neutral={neutral} bad={bad} all={metricas.all} average={metricas.average} positive={metricas.positive}/>
     </div>
   )
 
